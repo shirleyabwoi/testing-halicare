75bdc5f3ba84fe3bc555fabbd37090407f566041
habtamneshmarye <habtamneshmarye@gmail.com>
2025-09-06 18:15:13 +0300

    added arv model

--- arv/models.py
+++ arv/models.py
@@ -6,7 +6,6 @@ class ARVAvailability(models.Model):
         ("not available", "Not available"),
     ]
 
-    # Change 'clinics.Clinic' to 'halicare.Clinic' or your actual app label
     clinic = models.ForeignKey("center.Center", on_delete=models.CASCADE, related_name="arv_availabilities")
     arv_availability = models.CharField(max_length=20, choices=Availability_CHOICES)
     last_updated = models.DateTimeField(auto_now=True)
